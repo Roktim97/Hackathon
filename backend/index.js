ccda83c5efdd3ab8e52d935efa278de81cb16d74
Roktim97 <roktim@impactweaver.com>
2024-05-09 16:36:16 +0530

    basic server initialisation

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require("mongoose")
+const cors = require('cors')
+
+require('dotenv').config()
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true, multipart: true }))
+const corsOptions = {
+    origin: JSON.parse(process.env.allowedUrls),
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}
+
+mongoose
+  .connect(process.env.mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('connected to DB')
+  })
+  .catch((err) => {
+    console.log(err.message)
+  })
+
+app.use(cors(corsOptions))
+
+app.listen(3000, ()=>{
+    console.log("Server is running")
+})
